3242d161ea60443ea8c09b1afb083afe4b376e55
Bryamslm <bryamlopez1882002@estudiantec.cr>
2022-04-03 17:27:13 -0600

    ejercicio 2 imperativo 2

--- Paradigma_Imperativo/Parte II/ejercicio_2.go
+++ Paradigma_Imperativo/Parte II/ejercicio_2.go
@@ -47,13 +47,13 @@ func main() {
 	listaPersonas := make([]persona, 0)
 
 	listaPersonas = append(listaPersonas, persona{"Juan", [10]int{1,2,3,4,5,6,7,8,9,10}})
-	listaPersonas = append(listaPersonas, persona{"Pedro", [10]int{1,2,3,4,5,6,7,10,11}})
-	listaPersonas = append(listaPersonas, persona{"Maria", [10]int{1,2,3,4,5,6,7,10,11,12}})
+	listaPersonas = append(listaPersonas, persona{"Pedro", [10]int{1,2,3,4,5,6,7,8,10,11}})
 	listaPersonas = append(listaPersonas, persona{"Jose", [10]int{1,2,3,4,5,6,10,11,12,13}})
 	listaPersonas = append(listaPersonas, persona{"Carlos", [10]int{1,2,3,4,5,10,11,12,13,14}})
 	listaPersonas = append(listaPersonas, persona{"Bryam", [10]int{1,2,3,4,10,11,12,13,14,15}})
 	listaPersonas = append(listaPersonas, persona{"Bernal", [10]int{1,2,3,10,11,12,13,14,15,16}})
 	listaPersonas = append(listaPersonas, persona{"Karla", [10]int{1,2,3,10,11,12,13,14,15,17}})
+	listaPersonas = append(listaPersonas, persona{"Maria", [10]int{1,2,3,10,5,6,7,10,11,12}})
 
 	fmt.Println(masParecido(listaPersonas, [10]int{1,2,3,4,5,6,7,8,9,10}))
 	fmt.Println(masParecido(listaPersonas, [10]int{1,2,3,10,11,12,13,14,15,20}))
